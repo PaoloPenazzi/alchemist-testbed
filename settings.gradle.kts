601afbd2ff7b2b4e1d9eb5d6c7af410c4d6a353f
Paolo Penazzi <58599380+PaoloPenazzi@users.noreply.github.com>
2023-10-15 17:20:45 +0200

    chore: add others alchemist modules to the project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,37 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "alchemist-primer"
+include(
+    "alchemist-api",
+    "alchemist-cognitive-agents",
+    "alchemist-engine",
+    "alchemist-euclidean-geometry",
+    "alchemist-full",
+    "alchemist-graphql",
+    "alchemist-grid",
+    "alchemist-implementationbase",
+    "alchemist-incarnation-protelis",
+    "alchemist-incarnation-sapere",
+    "alchemist-incarnation-scafi",
+    "alchemist-incarnation-biochemistry",
+    "alchemist-loading",
+    "alchemist-maintenance-tooling",
+    "alchemist-maps",
+    "alchemist-multivesta-adapter",
+    "alchemist-physics",
+    "alchemist-sapere-mathexp",
+    "alchemist-smartcam",
+    "alchemist-swingui",
+    "alchemist-sapere-mathexp",
+    "alchemist-smartcam",
+    "alchemist-test",
+    "alchemist-ui-tooling",
+    "alchemist-swingui",
+    "alchemist-fxui",
+    "alchemist-web-renderer",
+)
+
+rootProject.name = "alchemist-testbed"
 
 gradleEnterprise {
     buildScan {
